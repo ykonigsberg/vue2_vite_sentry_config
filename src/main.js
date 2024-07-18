51efda4717d7bc7b2467f5c4d751c569c005d0eb
sergiosentry <109162568+serglom21@users.noreply.github.com>
2024-07-18 08:23:52 -0400

    set extra properties in indez initialization

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import * as Sentry from "@sentry/vue";
 import App from "./App.vue";
 import router from './router';
+import axios from 'axios';
 
 // tags, release , environemt, event.extra, event.user, transport, tags 
 // add session replay config (index.html if possible)
@@ -12,34 +13,21 @@ import router from './router';
 // npm run dev 
 
 Vue.use(router);
-// Sentry.init({
-//   Vue,
-//   environemt: "app",
-//   Release: "v1.0.0",
-//   dsn: "https://dc833b966ab24c74b407cc1005231e07@o4504514050326528.ingest.us.sentry.io/4504514051506176",
-//   integrations: [
-//     Sentry.replayIntegration(),
-//     Sentry.browserTracingIntegration({ router }),
-//   ],
-//   // Session Replay
-//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-// });
-// const { replayIntegration, browserTracingIntegration } = await import("@sentry/browser");
-// const clientOptions = {
-//   ...Sentry.options,
-//   dsn: "https://dc833b966ab24c74b407cc1005231e07@o4504514050326528.ingest.us.sentry.io/4504514051506176",
-//   integrations: [
-//     Sentry.replayIntegration(),
-//     Sentry.browserTracingIntegration({ router }),
-//   ],
-//   replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-//   replaysOnErrorSampleRate: 1.0, 
-// };
-// // Sentry.addIntegration(replayIntegration())
-// // Sentry.addIntegration(browserTracingIntegration({ router }))
-// const client = new Sentry.BrowserClient(clientOptions);
-// Sentry.getCurrentHub().bindClient(client);
+Sentry.init({
+  Vue,
+  environment: "app",
+  release: "v1.0.0",
+  dsn: DSN,
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.browserTracingIntegration({ router }),
+  ],
+
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+Sentry.captureException("Error in Vue")
 
 new Vue({
   router,
